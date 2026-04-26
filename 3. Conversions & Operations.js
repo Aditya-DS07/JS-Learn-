60cf9c7d31eb485f033e176c3c51b4d40caf626f
Aditya <adityabadhwar.dev@gmail.com>
2026-04-26 09:48:15 +0530

    L3 Conversions & Operations

---
+++ 3. Conversions & Operations.js
@@ -0,0 +1,52 @@
+// let score = "40"    // "40" => 40
+// let score2 = "12abc"   //"12abc" => NaN
+// let score3 = null       //null = 0
+// let score4 = undefined  //undefined = NaN
+// let score5 = "Aditya"   //Aditya = NaN
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// let valueInNumber1 = Number(score2)
+// let valueInNumber2 = Number(score3)
+// let valueInNumber3 = Number(score4)
+// let valueInNumber4 = Number(score5)
+
+// console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber1);
+// console.log(typeof valueInNumber2);
+// console.log(typeof valueInNumber3);
+// console.log(typeof valueInNumber4);
+
+// console.log(valueInNumber);
+// console.log(valueInNumber1);
+// console.log(valueInNumber2);
+// console.log(valueInNumber3);
+// console.log(valueInNumber4);
+
+
+
+
+// let isLoggedIn = 1
+// let bool = Boolean(isLoggedIn)
+// console.log(isLoggedIn)
+// console.log(bool) // true
+
+// let isLoggedOff = ""
+// let booll = Boolean(isLoggedOff)
+// console.log(isLoggedOff)
+// console.log(booll)  //false
+
+
+// let isLogged = "Aditya"
+// let boolle = Boolean(isLogged)
+// console.log(isLogged)
+// console.log(boolle)     //true
+
+
+
+
+
+let a = 33 
+let astring = String(a)
+console.log(astring)
+console.log(typeof astring)
