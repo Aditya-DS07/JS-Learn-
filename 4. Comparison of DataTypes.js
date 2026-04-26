578e1dce2877b519802511ea0405ef7a498bd041
Aditya <adityabadhwar.dev@gmail.com>
2026-04-26 12:19:12 +0530

    L3 Comparison of DataTypes

---
+++ 4. Comparison of DataTypes.js
@@ -0,0 +1,15 @@
+// console.log("2">1);
+// console.log("02">1);
+
+
+
+console.log(null>0);  
+console.log(null==0);
+console.log(null>=0);   // this is true because in JS comparison operators converts the null to a number (i.e 0) , hence it is true 
+
+
+
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined>=0);
+console.log(undefined==0);
