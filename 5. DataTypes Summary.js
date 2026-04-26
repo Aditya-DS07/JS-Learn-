77a59cd852915374cafc838b360196bb706f0fcc
Aditya <adityabadhwar.dev@gmail.com>
2026-04-26 12:56:35 +0530

    L5 Summary of DataTypes

---
+++ 5. DataTypes Summary.js
@@ -0,0 +1,20 @@
+// Primitive
+    // 7 Types: 
+            // 1. String        "abd12"
+            // 2. Number        12
+            // 3. Boolean       true/false
+            // 4. null          null
+            // 5. undefined     undefined
+            // 6. symbol        gives a unique id to wahtever it is assigned to 
+            // 7. bigint        used to represent very big number (1234343234567898765434567898765434567876543223456n)
+
+
+// Non Primitive / Reference Type
+    // 1. Array     names = ["aditya", "rohan"
+    // 2. Objects   let objs = {
+    //                     name: "aditya"
+    //                     age : 18
+    //              }
+    // 3. Functions     const myfunc = function() {
+    //                        console.log("hello");
+    //                  } 
