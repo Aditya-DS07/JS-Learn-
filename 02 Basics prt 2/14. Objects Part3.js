0239b337b0665ab815e65169e7588684aa00df07
Aditya <adityabadhwar.dev@gmail.com>
2026-04-30 11:41:55 +0530

    L14 Objects Part 3

---
+++ 02 Basics prt 2/14. Objects Part3.js
@@ -0,0 +1,13 @@
+const course = {
+    name: "JavaScript Course",
+    price: "free",
+    mentor: "self"
+}
+
+
+// console.log(course["mentor"]);
+
+const {mentor:teacher} = course   
+
+console.log(teacher)
+
