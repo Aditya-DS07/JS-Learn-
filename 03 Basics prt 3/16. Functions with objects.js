d4b20e2bfbde73ee34d13df3883993b1c516c6cc
Aditya <adityabadhwar.dev@gmail.com>
2026-05-02 12:10:54 +0530

    L16 Functions with objects

---
+++ 03 Basics prt 3/16. Functions with objects.js
@@ -0,0 +1,30 @@
+// function calculateprice(...numbe){
+//     return numbe
+// }
+// console.log(calculateprice(200,32,2,2323,23))
+
+const user = {
+    name: "aditya",
+    age: 18
+}
+
+function userr(someuser){
+    console.log(`username is ${someuser.name} and age is ${someuser.age}`)
+}
+
+// userr(user)
+
+// userr({
+//     name: "aditya",
+//     age: 18
+// }
+// )
+
+
+const newarr = [12,324,32]
+
+function returnvalue(arr){
+    return arr[0]
+}
+// console.log(returnvalue(newarr))
+console.log(returnvalue([12,323,32]))
