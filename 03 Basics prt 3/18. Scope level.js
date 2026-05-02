953f678b68a814db66837ab82d2e954aeb9af4df
Aditya <adityabadhwar.dev@gmail.com>
2026-05-02 18:53:44 +0530

    L18 Scope level

---
+++ 03 Basics prt 3/18. Scope level.js
@@ -0,0 +1,37 @@
+function one(){
+    const username = "aditya"
+    function two(){
+        const age = 18
+        console.log(username);
+        
+    }    
+    // console.log(age);
+    two()
+}
+// one()
+
+
+
+if (true) {
+    const username = "aditya"
+    if (username === "aditya") {
+        const age = " 18"
+        // console.log(username+age)
+    }
+    // console.log(age);
+    
+}
+// console.log(username);
+
+
+
+function addone(num){
+    return num+1
+}
+console.log(addone(5))
+
+const addtwo = function(num){
+    return num+2
+}
+console.log(addtwo(5))
+
